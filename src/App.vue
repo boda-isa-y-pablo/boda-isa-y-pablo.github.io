d36e56f83e98a88e9c22d06ace63acec2b29297b
Pablo Castillo <carp_97@outlook.com>
2025-09-09 22:41:47 -0600

    fix: build issue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
-  <!-- <InvitationV1 /> -->
   <InvitationV2 />
 </template>
 
 <script setup lang="ts">
-import InvitationV1 from './components/InvitationV1.vue';
 import InvitationV2 from './components/InvitationV2.vue';
 </script>
